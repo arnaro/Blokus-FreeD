9a14dc4d98389202f5594acd1f1c4494a914f9e1
Vifill <vifill12@gmail.com>
2013-09-20 15:56:42 +0000

    BlockFactory.cs

---
+++ Blokus/PieceFactory.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blokus.Model;
+
+namespace Blokus
+{
+    class PieceFactory
+    {
+        static public IList<IPiece> GetPieces()
+        {
+            IList<IPiece> ListOfPieces = new List<IPiece>();
+
+            var baseForm = new byte[,]
+                {
+                    {1}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+
+            baseForm = new byte[,]
+                {
+                    {1,0},
+                    {1,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1},
+                    {1,1}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,1,0},
+                    {0,1,0},
+                    {0,1,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,1,0,0},
+                    {0,1,0,0},
+                    {0,1,0,0},
+                    {0,1,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,0,1,0,0},
+                    {0,0,1,0,0},
+                    {0,0,1,0,0},
+                    {0,0,1,0,0},
+                    {0,0,1,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1},
+                    {0,1}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1,1},
+                    {0,0,1},
+                    {0,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1,1},
+                    {0,1,0},
+                    {0,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1,1},
+                    {1,0,1},
+                    {0,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,1,0},
+                    {1,1,1},
+                    {0,1,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1,0},
+                    {0,1,1},
+                    {0,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,0,1},
+                    {1,1,1},
+                    {1,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1,1},
+                    {1,1,0},
+                    {0,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,0,0},
+                    {1,1,1},
+                    {0,1,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,1,1},
+                    {1,1,0},
+                    {1,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1,1},
+                    {0,1,0},
+                    {0,1,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {1,1,1},
+                    {0,0,1},
+                    {0,0,1}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,0,0,0},
+                    {1,1,1,1},
+                    {0,0,0,1},
+                    {0,0,0,0}
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,0,0,0},
+                    {1,1,1,1},
+                    {0,0,1,0},
+                    {0,0,0,0}
+                    
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+            baseForm = new byte[,]
+                {
+                    {0,0,0,0},
+                    {1,1,1,0},
+                    {0,0,1,1},
+                    {0,0,0,0}
+                    
+                };
+            ListOfPieces.Add(new Piece(baseForm));
+
+            return ListOfPieces;
+        }
+    }
+}
