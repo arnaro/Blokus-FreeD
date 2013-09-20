3567f9cd85d88561898d489aa1a2a998cefe9697
Vifill <vifill12@gmail.com>
2013-09-20 15:08:44 +0000

    Block.cs

---
+++ Blokus/BlockTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blokus.Model;
+using NUnit.Framework;
+
+namespace Blokus
+{
+    [TestFixture]
+    class BlockTests
+    {
+        [Test]
+        public void BlockFormTest()
+        {
+            var basicForm = new byte[1,1];
+            basicForm[0,0] = 1;
+
+            Block block = new Block(basicForm);
+            Assert.AreEqual(8,block.ListRoations().Count);
+
+            basicForm = new byte[2,2]
+                {
+                    {1, 1},
+                    {1, 0}
+                };
+            var Form2 = new byte[2, 2]
+                {
+                    {1, 1},
+                    {0, 1}
+                };
+
+            block = new Block(basicForm);
+            Assert.AreEqual(Form2, block.ListRoations()[1]);
+
+            var bigBaseForm = new byte[4, 4]
+                {
+                    {0, 0, 1, 0},
+                    {0, 1, 1, 0},
+                    {0, 1, 0, 0},
+                    {0, 1, 0, 0},
+                };
+            var formFlip = new byte[4, 4]
+                {
+                    {0, 1, 0, 0},
+                    {0, 1, 1, 0},
+                    {0, 0, 1, 0},
+                    {0, 0, 1, 0},
+                };
+            block = new Block(bigBaseForm);
+            Assert.AreEqual(formFlip, block.ListRoations()[4]);
+        }
+    }
+}
---
+++ Blokus/Model/Block.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blokus.Model
+{
+    class Block
+    {
+        protected byte[,] baseForm;
+        private int size;
+
+        public Block(byte[,] matrix)
+        {
+            baseForm = matrix;
+            size = (int) Math.Sqrt(baseForm.Length);
+        }
+
+        public List<byte[,]> ListRoations()
+        {
+            var array = new List<byte[,]>(8);
+            array.Add(baseForm);
+            int i;
+            for (i = 1; i < 4; i++)
+            {
+                array.Add(RotateMatrix(array[i-1]));
+            }
+            var flippedForm = FlipMatrix(baseForm);
+            array.Add(flippedForm);
+            for (i = 5; i < 8; i++)
+            {
+                array.Add(RotateMatrix(array[i - 1]));
+            }
+            return array;
+        }
+
+        byte[,] FlipMatrix(byte[,] matrix)
+        {
+            var flippedMatrix = new byte[size,size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    flippedMatrix[i, size - j - 1] = matrix[i, j];
+                }
+            }
+            return flippedMatrix;
+        }
+
+        public void PruneForms()
+        {
+            
+        }
+
+        byte[,] RotateMatrix(byte[,] matrix)
+        {
+            byte[,] ret = new byte[size, size];
+
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    ret[i, j] = matrix[size - j - 1, i];
+                }
+            }
+            return ret;
+        }
+    }
+}
